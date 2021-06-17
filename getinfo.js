1d77f26c3714a946f7daf63d6866abe62e196ef5
Danny <dannyho77@gmail.com>
2021-06-17 13:50:38 -0400

    rendering info on page

---
+++ getinfo.js
@@ -0,0 +1,10 @@
+const addInfo,(string) => {
+    // create a new div element
+    const newDiv = document.createElement("div");
+  
+    // and give it some content
+    const newContent = document.createTextNode("Hi there and greetings!");
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+};
